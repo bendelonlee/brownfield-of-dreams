370664c2de8b4f1acf306359b6537bb51836e4a8
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-02 15:42:08 -0700

    Updates test for connecting github on dashboard page.

--- spec/features/user/github/user_can_connect_to_github_spec.rb
+++ spec/features/user/github/user_can_connect_to_github_spec.rb
@@ -1,13 +1,34 @@
 require 'rails_helper'
 
 describe 'as a user on the dashboard screen' do
-  xit 'can connect to Github' do
-    Capybara.app = OauthWorkshop::Application
+  it 'can connect to Github' do
+    stub_omniauth
+    user = create(:user)
+		allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
+    VCR.use_cassette("services/find_repositories") do
+      VCR.use_cassette("services/find_followers") do
+        VCR.use_cassette("services/find_followings") do
+          visit '/dashboard'
+        end
+      end
+    end
 
-    click_on 'Connect Your Github'
+		expect(page).to have_link("Connect Your Github")
 
+		click_on "Connect Your Github"
+    save_and_open_page
 
+		expect(page).to_not have_link("Connect Your Github")
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = nil
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      "uid" => "12345",
+      "provider" => "github",
+      "credentials" => {
+        "token" => "12345"}})
   end
 end
