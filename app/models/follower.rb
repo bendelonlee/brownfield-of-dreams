a52fcda0032145cc2aad38289c8ff09e435e4756
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-03 18:37:54 -0700

    Adds id to the Follower & Following model so we can access them for the friendships I am going to add.

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,8 +1,9 @@
 class Follower
-  attr_reader :url, :name
+  attr_reader :url, :name, :follower_id
 
   def initialize(data)
     @url = data[:html_url]
     @name = data[:login]
+    @follower_id = data[:id]
   end
 end
--- app/models/following.rb
+++ app/models/following.rb
@@ -1,8 +1,9 @@
 class Following
-  attr_reader :url, :name
+  attr_reader :url, :name, :following_id
 
   def initialize(data)
     @url = data[:html_url]
     @name = data[:login]
+    @following_id = data[:id]
   end
 end
