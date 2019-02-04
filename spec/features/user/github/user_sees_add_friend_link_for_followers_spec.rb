92df84c722ed1529014adc6fe2d1bf32fa50983a
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-03 18:33:19 -0700

    Updates test to be more clear.

--- spec/features/user/github/user_sees_add_friend_link_for_followers_spec.rb
+++ spec/features/user/github/user_sees_add_friend_link_for_followers_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe "as a logged in user" do
   describe "on the dashboard" do
-    it "shows add friend link next to followers who are in your database with github token" do
+    it "shows add friend link next to followers who are in your database with a github token" do
       current_user = create(:user, github_token: ENV["GITHUB_API_KEY"])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
 
-      anten = create(:user, github_uid: 'cebarks')
-      nico = create(:user, github_uid: 'nicovigil1')
+      create(:user, github_uid: 'cebarks')
+      create(:user, github_uid: 'nicovigil1')
+      create(:user, github_uid: 'asmolentzov')
 
       VCR.use_cassette("services/find_repositories") do
         VCR.use_cassette("services/find_followers") do
@@ -19,13 +20,22 @@ describe "as a logged in user" do
 
       expect(current_path).to eq('/dashboard')
       expect(page).to have_content("Followers:")
-      within('#cebarks') do
+      save_and_open_page
+      within('#follower-cebarks') do
         expect(page).to have_link("cebarks")
-        expect(page).to have_link("Add as Friend")
+        expect(page).to have_content("Add as Friend")
       end
-      within('#nicovigil1') do
+      within('#following-asmolentzov') do
+        expect(page).to have_link("asmolentzov")
+        expect(page).to have_content("Add as Friend")
+      end
+      within('#follower-nicovigil1') do
         expect(page).to have_link("nicovigil1")
-        expect(page).to_not have_link("Add as Friend")
+        expect(page).to_not have_content("Add as Friend")
+      end
+      within('#following-mgoodhart5') do
+        expect(page).to have_link("mgoodhart5")
+        expect(page).to_not have_content("Add as Friend")
       end
     end
   end
