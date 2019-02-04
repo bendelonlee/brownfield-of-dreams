451cc71d0ac8af671d39a616340c2401bfb3dd83
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-03 22:39:10 -0700

    Updates relationships in friendship and user model.  Also, adds methods for git_hub_facade methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
-  has_many :followed_users, foreign_key: :follower_id, class_name: 'Friendship'
-  has_many :followees, through: :followed_users
-
-  has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
-  has_many :followers, through: :following_users
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: "User"
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, if: :password
@@ -18,5 +15,9 @@ class User < ApplicationRecord
     Tutorial.joins(videos: :users)
             .where(users: {id: self.id})
             .group(:id)
+  end
+
+  def self.find_friend_id(name)
+    User.where(github_uid: name).pluck(:id).first
   end
 end
